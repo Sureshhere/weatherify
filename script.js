90646cfb56fb16a3c0fd0d9e0ba20ff6e9dd207e
Suresh <2111cs030133@mallareddyuniversity.ac.in>
2023-02-21 18:54:59 +0530

    scroll to top

--- script.js
+++ script.js
@@ -114,5 +114,24 @@ var Snow = {
 
 var confetti = Snow.init();
 
-  // --------------------------
+// --------------------------
 
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
