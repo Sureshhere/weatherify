d3df4a889ecf4199e822e424583ca4ca393035fa
Suresh <2111cs030133@mallareddyuniversity.ac.in>
2023-05-05 21:05:05 +0530

    added vercel analytics

--- script.js
+++ script.js
@@ -135,3 +135,18 @@ function toTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// --------------
+
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+ 
+export default MyApp;
